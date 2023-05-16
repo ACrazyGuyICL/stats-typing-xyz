723b3b97e7b0537743bf283a89eddde9e60c4567
ACrazyGuyICL <123118443+ACrazyGuyICL@users.noreply.github.com>
2023-05-16 19:07:55 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors()); // Enable CORS for all routes
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log('Server started on port 3000');
 });
 
